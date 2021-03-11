7a82bc12d8708f623d8d22b77e8aadc4bcb03fbc
Ayush Singhal <1998ayushsinghal@gmail.com>
2021-03-11 21:30:18 +0530

    filter part AND localstorage for cart

--- new_body_care.js
+++ new_body_care.js
@@ -9,4 +9,58 @@ help_btn[0].addEventListener('click',()=>{
     }
     
 })
+var cart_data = [];
+async function add(){
+    var btnid = event.target.id
+    let fet = fetch(" http://localhost:3000/new_body")
+    let data = await fet
+    let arr = await data.json();
+    for(let i = 0 ;i<16;i++){
+        if(`btn${arr[i].id}` == btnid){
+            cart_data.push(arr[i])
+        }
+    }
+    let json = JSON.stringify(cart_data)
+    localStorage.setItem("cart",json)
+}
+var expanded = false;
 
+function showCheckboxes1() {
+  let checkboxes = document.getElementById("ptcheckboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    checkboxes.style.position="absolute"
+    checkboxes.style.backgroundColor="white"
+    checkboxes.style.borderBottom = "2px solid black"
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+function showCheckboxes2() {
+    let checkboxes = document.getElementById("fncheckboxes");
+    if (!expanded) {
+      checkboxes.style.display = "block";
+      checkboxes.style.position="absolute"
+    checkboxes.style.backgroundColor="white"
+    checkboxes.style.borderBottom = "2px solid black"
+
+      expanded = true;
+    } else {
+      checkboxes.style.display = "none";
+      expanded = false;
+    }
+  }function showCheckboxes3() {
+    var checkboxes = document.getElementById("fccheckboxes");
+    if (!expanded) {
+      checkboxes.style.display = "block";
+      checkboxes.style.backgroundColor="white"
+      checkboxes.style.borderBottom = "2px solid black"
+      checkboxes.style.position="absolute"
+      expanded = true;
+    } else {
+      checkboxes.style.display = "none";
+      expanded = false;
+    }
+  }

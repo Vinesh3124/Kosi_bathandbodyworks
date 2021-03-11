553d3673e3982155b64fa22f6e7fe6ba7c19cc0f
Ayush Singhal <1998ayushsinghal@gmail.com>
2021-03-11 13:32:25 +0530

    some updates

--- new_body_care.js
+++ new_body_care.js
@@ -1,9 +1,12 @@
+
+    
 let help_btn = document.getElementsByClassName('help-btn');
 let cols = document.getElementsByClassName('col')
-​
+
 help_btn[0].addEventListener('click',()=>{
-    for(var i=1;i<=4;i++){
+    for(let i=1;i<=4;i++){
         cols[i].classList.toggle("open-end-nav")
     }
     
 })
+

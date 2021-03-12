b9b436efd7c6846223773d9bda51220e02a9f542
Ayush Singhal <1998ayushsinghal@gmail.com>
2021-03-12 12:29:51 +0530

    database parsing the count

--- body care/new_body_care.js
+++ body care/new_body_care.js
@@ -9,19 +9,65 @@ help_btn[0].addEventListener('click',()=>{
     }
     
 })
-var cart_data = [];
-async function add(){
-    var btnid = event.target.id
-    let fet = fetch(" http://localhost:3000/new_body")
-    let data = await fet
-    let arr = await data.json();
+let arr = []
+function add(event){
+
+    let btnid = event.target.id
+    fetch(`http://localhost:3000/new_body`).then(response => response.json()).then(data => alldata(data)).catch(error => console.log(error))
+  function alldata(data){
     for(let i = 0 ;i<16;i++){
-        if(`btn${arr[i].id}` == btnid){
-            cart_data.push(arr[i])
-        }
+      if(`btn${data[i].id}` == btnid){
+          var id = data[i].id
+      }
     }
-    let json = JSON.stringify(cart_data)
-    localStorage.setItem("cart",json)
+
+    fetch(`http://localhost:3000/new_body?id=${id}`).then(response => response.json()).then(data => getProductDetailsCart(data)).catch(error => console.log(error))
+    
+    function getProductDetailsCart(data){
+        let html = ""
+        let id = data[0].id
+        let name = data[0].name
+        let img = data[0].img
+        let price = data[0].Price
+        let shr_Desc = data[0].Short_Description
+        let count = data[0].count
+        let total = data[0].totalP
+        console.log(total)
+        let temp = {}
+        temp.id = id
+        temp.name = name
+        temp.img = img
+        temp.price = price
+        temp.desc = shr_Desc
+        temp.count = count
+        temp.total = total
+
+        console.log(temp)
+        arr = [...arr,temp]
+        localStorage.setItem("cart",JSON.stringify(arr))
+        let modal1 = document.getElementById("myModal1");
+        let span1 = document.getElementsByClassName("close1")[0];
+        modal1.style.display = "block";
+        span1.onclick = function() {
+            modal1.style.display = "none";
+        }
+        window.onclick = function(event) {
+            if (event.target == modal1) {
+                modal1.style.display = "none";
+            }
+        }
+
+        html+= `
+        <div class="cartAdd">
+            <h3>Item Added to Cart</h3>
+            <div class="imageBox">
+                <img src="https://media.tenor.com/images/b95474b4e57295c82fb7ffc3b882e683/tenor.gif" class="img" alt="">
+            </div>
+        </div>
+        `
+        document.getElementById("cartAddModal").innerHTML = html
+    }
+  }
 }
 var expanded = false;
 
@@ -63,11 +109,4 @@ function showCheckboxes2() {
       checkboxes.style.display = "none";
       expanded = false;
     }
-  }
-
-
-  function quicklook(){
-    let div = document.createElement("div")
-    
-        // <i class="fa fa-eye" style="font-size:36px"></i>
   }
